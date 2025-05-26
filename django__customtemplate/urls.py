18b023c548d694814d20a6e2827fd7c23ae97e39
unmatched78 <marvinavi24@gmail.com>
2025-05-26 09:41:42 +0200

    cleaning

--- django__customtemplate/urls.py
+++ django__customtemplate/urls.py
@@ -1,7 +1,5 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
 # project/urls.py
 
 urlpatterns = [
@@ -9,8 +7,10 @@ urlpatterns = [
     path('', include('core.urls')),
 ]
 
-# serve static files and media
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# from django.conf.urls.static import static
+# from django.conf import settings
+# # serve static files and media
+# urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+# urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
