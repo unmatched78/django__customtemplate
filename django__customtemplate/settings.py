fcfe0f3e7ac499d84c06fd3e95548b3b31b5f931
unmatched78 <marvinavi24@gmail.com>
2025-05-26 10:02:42 +0200

    setting logic

--- django__customtemplate/settings.py
+++ django__customtemplate/settings.py
@@ -77,7 +77,22 @@ if DEBUG == True:
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
-   }
+   }, 
+   
+   AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
 else:
     # Load the DATABASE_URL from environment variables
     DATABASES = {
