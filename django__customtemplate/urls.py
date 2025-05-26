914d715307b9f08ec01b402194d0b128fd0a4f82
unmatched78 <marvinavi24@gmail.com>
2025-05-26 10:35:45 +0200

    satitic file handling

--- django__customtemplate/urls.py
+++ django__customtemplate/urls.py
@@ -7,10 +7,14 @@ urlpatterns = [
     path('', include('core.urls')),
 ]
 
-# from django.conf.urls.static import static
-# from django.conf import settings
-# # serve static files and media
-# urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-# urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+from django.conf.urls.static import static
+from django.conf import settings
+from settings import DEBUG
+# handle static files
+    
+if DEBUG:
+    # serve static files and media
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
