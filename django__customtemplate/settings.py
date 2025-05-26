d3336c9390696ca5ffac3c31b0314ca037ee1bd9
unmatched78 <marvinavi24@gmail.com>
2025-05-26 09:39:29 +0200

    cleaning

--- django__customtemplate/settings.py
+++ django__customtemplate/settings.py
@@ -89,7 +89,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587  
 EMAIL_USE_TLS = True  
 EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')  # Your Gmail address  
-EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')  # Use an App Password for increased security  
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')  # Use an App Password 
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 AUTH_USER_MODEL = 'core.CustomUser'  
