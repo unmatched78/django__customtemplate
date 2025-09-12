1342248be81f01b2a9c99984dfe7cef174bcaff7
Viella <168560797+unmatched78@users.noreply.github.com>
2025-09-12 23:59:17 +0200

    Update dev_ment.py

--- django__customtemplate/settings/dev_ment.py
+++ django__customtemplate/settings/dev_ment.py
@@ -6,3 +6,13 @@ SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 DEBUG = os.getenv('DEBUG_STATE', 'False').lower() == 'true'
 
 ALLOWED_HOSTS = ["*"]
+DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
+STATIC_URL = "static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+MEDIA_URL = "/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
