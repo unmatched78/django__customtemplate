2ee6e31dafe91d0a25fe31b0b6ec2961dab443af
Viella <168560797+unmatched78@users.noreply.github.com>
2025-09-12 23:55:43 +0200

    Update settings.py

--- django__customtemplate/settings.py
+++ django__customtemplate/settings.py
@@ -9,16 +9,6 @@ import cloudinary
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
-
-# SECURITY WARNING: don't run with debug turned on in production!
-# Convert DEBUG to boolean
-DEBUG = os.getenv('DEBUG_STATE', 'False').lower() == 'true'
-
-ALLOWED_HOSTS = ["*"]
-
-
 # Application definition
 
 INSTALLED_APPS = [
