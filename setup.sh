617fae89c88beb79b8c3b54382e924796917d072
Viella <168560797+unmatched78@users.noreply.github.com>
2025-09-13 00:13:14 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,1 +1,12 @@
+#! usr/bin/env bash 
+set -e
+ echo "Starting installing👌😒😒"
 
+ python3 -m venv venv
+ source venv/bin/activate 
+ echo "environment set successfully 😎😎😎"
+ pip install -r requirements.txt
+ touch .gitignore
+
+ echo "finished setup 🤛"
+ 
