619b7b6c9a1705bcf238a9411c7f5a0ebfa4a2d6
Viella <168560797+unmatched78@users.noreply.github.com>
2025-09-12 23:56:20 +0200

    Update dev_ment.py

--- django__customtemplate/settings/dev_ment.py
+++ django__customtemplate/settings/dev_ment.py
@@ -1,3 +1,7 @@
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
+
+# SECURITY WARNING: don't run with debug turned on in production!
 # Convert DEBUG to boolean
 DEBUG = os.getenv('DEBUG_STATE', 'False').lower() == 'true'
 
